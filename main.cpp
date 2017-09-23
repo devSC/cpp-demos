fadf421f1d6bff9a4f352ec7f8c366a71f9376db
Wilson <xiaochong2154@163.com>
2017-09-23 17:28:45 +0800

    [UPDATE PTHREAD]

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@
 #include <map>
 #include <algorithm>
 #include <fstream>
+#include <pthread.h>
 
 //声明使用std命名控件，下面凡是用到标准库的方法属性都不需要使用std::去使用
 //std 是所有标准库的命名空间的名字，
@@ -90,6 +91,10 @@ void testFileOutput();
 //指针
 void testPointer();
 
+
+//多线程
+void testpthread();
+
 int main() {
 
 //    testPrintf();
@@ -128,7 +133,9 @@ int main() {
 
 //    testFileOutput();
 
-    testPointer();
+//    testPointer();
+
+    testpthread();
 
     return 0;
 }
@@ -1102,5 +1109,82 @@ void testReturnPointerFromFunction() {
 }
 
 
+/*
+ * 多线程 http://www.runoob.com/cplusplus/cpp-multithreading.html
+ *
+ * 多线程是多任务处理的一种特殊形式，多任务处理允许让电脑同时运行两个或两个以上的程序。一般情况下，两种类型的多任务处理：基于进程和基于线程。
+    基于进程的多任务处理是程序的并发执行。
+    基于线程的多任务处理是同一程序的片段的并发执行。
+
+    多线程程序包含可以同时运行的两个或多个部分。这样的程序中的每个部分称为一个线程，每个线程定义了一个单独的执行路径。
+ */
+
+
+
+void testpthread1();
+
+void testpthread() {
+    /*
+    pthread_create (thread, attr, start_routine, arg)
+
+    在这里，pthread_create 创建一个新的线程，并让它可执行。下面是关于参数的说明：
+    参数      	描述
+    thread	指向线程标识符指针。
+    attr	一个不透明的属性对象，可以被用来设置线程属性。您可以指定线程属性对象，也可以使用默认值 NULL。
+    start_routine	线程运行函数起始地址，一旦线程被创建就会执行。
+    arg	运行函数的参数。它必须通过把引用作为指针强制转换为 void 类型进行传递。如果没有传递参数，则使用 NULL。
+
+    创建线程成功时，函数返回 0，若返回值不为 0 则说明创建线程失败。
+     */
+    testpthread1();
+}
+
+#define NUM_THREADS 5
+//线程的运行函数
+void* say_hello(void *args);
+
+void testpthread1() {
+    /*
+  pthread_create (thread, attr, start_routine, arg)
+
+  在这里，pthread_create 创建一个新的线程，并让它可执行。下面是关于参数的说明：
+  参数      	描述
+  thread	指向线程标识符指针。
+  attr	一个不透明的属性对象，可以被用来设置线程属性。您可以指定线程属性对象，也可以使用默认值 NULL。
+  start_routine	线程运行函数起始地址，一旦线程被创建就会执行。
+  arg	运行函数的参数。它必须通过把引用作为指针强制转换为 void 类型进行传递。如果没有传递参数，则使用 NULL。
+
+  创建线程成功时，函数返回 0，若返回值不为 0 则说明创建线程失败。
+   */
+
+    // 定义线程的 id 变量，多个变量使用数组
+    pthread_t tids[NUM_THREADS];
+    int indexes[NUM_THREADS];// 用数组来保存i的值
+    int rc;
+    int i;
+    for (int i = 0; i < NUM_THREADS; ++i) {
+        cout << "main() : 创建线程, " << i << endl;
+        indexes[i] = i; //先保存i的值
+        //参数依次是: 创建的线程id, 线程参数, 调用的函数, 传入的函数参数.
+        // 传入的时候必须强制转换为void* 类型，即无类型指针
+        int result = pthread_create(&tids[i], NULL, say_hello, (void *)&(indexes[i]));
+        //创建线程成功时，函数返回 0，若返回值不为 0 则说明创建线程失败。
+        if (result != 0) {
+            cout << "pthread_create error: error_code=" << result << endl;
+            exit(-1);
+        }
+    }
+
+    //等各个线程退出后，进程才结束，否则进程强制结束了，线程可能还没反应过来；
+    pthread_exit(NULL);
+}
+
+void *say_hello(void *threadid) {
+
+    // 对传入的参数进行强制类型转换，由无类型指针变为整形数指针，然后再读取
+    int tid = *((int *)threadid);
+    cout << "Hello. 线程 ID, " << tid << endl;
+    pthread_exit(NULL);
+}
 
 
